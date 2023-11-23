1f3f2ab3490bb6ae69d93fe278f97c03fd5dea62
Estelle-Lee <bokyunglee33@gmail.com>
2023-11-23 19:16:45 +0000

    Week4 Quize

---
+++ 1-Fundamentals/Week4/challengeQeustions_oop.py
@@ -0,0 +1,19 @@
+'''
+What is the purpose of the __init__ method in a class? When does it execute? 
+>>> It is a constructor method used to declare and initialise instance attributes from a class. 
+It executes when object created
+
+What is the purpose of the self parameter in a method? 
+>>> It refers the object being instantiated. 
+Used for modifing attributes and methods of the instance within the method.
+
+What is an instance?
+>>> Instance is an object from class like functions.
+
+How do you define a subclass from a superclass?
+>>> Subclass can be define outside the superclass, inherits the superclass instances.
+Subclass have parameter of superclass.
+
+What is the use of the super() function?
+>>> super() function is used in the subclass to use superclass's instance in childclass.
+'''
